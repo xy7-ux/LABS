470f31509da9b5d1a6655c3ba7d6ee26393b8332
xyz 123 <ostrowskiszkola8@gmail.com>
2024-09-28 12:10:24 +0200

    Update Rock_paper_scissors.py

--- Python/Basic_Python_Projects/Rock_paper_scissors.py
+++ Python/Basic_Python_Projects/Rock_paper_scissors.py
@@ -28,54 +28,66 @@ scissors = '''
 ---.__(___)
 '''
 
+game_images = [rock, paper, scissors]
+
 user_choice = int(input("Welcome in the game choose 0 for rock, 1 for paper, 2 for scissors "))
+print(game_images[user_choice])
+
 computer_choice = random.randint(0, 2)
-if user_choice == 1 and computer_choice == 0:
-    print("your choice"
-          '''
-    _______
----'   ____)____
-          ______)
-          _______)
-         _______)
----.__________)
-'''
-          "computer choice: "
-'''  
-    _______
----'   ____)
-      (_____)
-      (_____)
-      (____)
----.__(___)
-'''
-          "you win")
-elif user_choice == 1 and computer_choice == 1:
-    print("play again you both choose paper")
-elif user_choice == 1 and computer_choice == 2:
-    print("you loose")
+print("Computer chose: ")
+print(game_images[user_choice])
 
-
-if user_choice == 0 and computer_choice == 0:
-    print("play again you both choose rock")
-elif user_choice == 0 and computer_choice == 1:
-    print("you loose")
+if user_choice >= 3 or user_choice < 0:
+    print("you typed wrong number. You loseee")
 elif user_choice == 0 and computer_choice == 2:
-    print("you win")
+    print("You win!")
+elif user_choice == 2 and computer_choice == 0:
+    print("You lose!")
+elif computer_choice > user_choice:
+    print("You lose!")
+elif computer_choice < user_choice:
+    print("You win!")
+elif computer_choice == user_choice:
+    print("its a draw")
 
 
-if user_choice == 2 and computer_choice == 0:
-    print("you loose")
-elif user_choice == 2 and computer_choice == 1:
-    print("you win")
-elif user_choice == 2 and computer_choice == 2:
-    print("play again you both choose sccisors")
 
-    # computer_choice = random.randint(0, 2)
-    # if computer_choice == 0:
-    # print("computer choose rock")
-    # elif computer_choice == 1:
-    # print("computer choose paper")
-    # else:
-    # print("computer choose scissors")
+
+
+
+
+# if user_choice == 1 and computer_choice == 0:
+#     print(f"Computer choice {computer_choice}")
+# elif user_choice == 1 and computer_choice == 1:
+#     print("play again you both choose paper")
+# elif user_choice == 1 and computer_choice == 2:
+#     print("you loose")
+# else:
+#     print("you typed wrong number.")
+#
+# if user_choice == 0 and computer_choice == 0:
+#     print("play again you both choose rock")
+# elif user_choice == 0 and computer_choice == 1:
+#     print("you loose")
+# elif user_choice == 0 and computer_choice == 2:
+#     print("you win")
+# else:
+#     print("you typed wrong number.")
+#
+#
+# if user_choice == 2 and computer_choice == 0:
+#     print("you loose")
+# elif user_choice == 2 and computer_choice == 1:
+#     print("you win")
+# elif user_choice == 2 and computer_choice == 2:
+#     print("play again you both choose sccisors")
+# else:
+#     print("you typed wrong number.")
+#     # computer_choice = random.randint(0, 2)
+#     # if computer_choice == 0:
+#     # print("computer choose rock")
+#     # elif computer_choice == 1:
+#     # print("computer choose paper")
+#     # else:
+#     # print("computer choose scissors")
 
