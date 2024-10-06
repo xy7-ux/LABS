101675db1fbdf4aefe60e9e4543ea4cedcfb982b
xyz 123 <ostrowskiszkola8@gmail.com>
2024-10-06 20:12:07 +0200

    Add files via upload

---
+++ Apps/I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Patryk Ostrowski on 06/10/2024.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
